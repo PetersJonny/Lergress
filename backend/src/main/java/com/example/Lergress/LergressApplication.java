c3d52893b93e16893fa1508a877d8a0b57f4a337
Peters <pedrojulio.barreto@gmail.com>
2025-10-26 16:32:19 -0300

    Update: organizing folders

---
+++ backend/src/main/java/com/example/Lergress/LergressApplication.java
@@ -0,0 +1,13 @@
+package com.example.Lergress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LergressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LergressApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/Lergress/LergressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Lergress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LergressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
